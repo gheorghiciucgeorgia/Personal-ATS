a90ee511c2d743e9f5b6bce2b4e2392925ad3c86
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-11-17 21:08:12 +0200

    vercel build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-    watch: {
-      usePolling: true,
-      interval: 1000  // Check la fiecare secundă
-    },
-    hmr: {
-      host: 'localhost',
-      port: 3000
-    }
-  }
+  plugins: [react()],
 })
