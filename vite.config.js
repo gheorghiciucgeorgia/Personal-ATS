a58263eb0126b7a1673929fb35b17a8b61f1f79d
Gheorghiciuc Georgia <80405885+gheorghiciucgeorgia@users.noreply.github.com>
2025-11-17 20:33:11 +0200

    vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
-        port: 3000
-    },
-    optimizeDeps: {
-        include: ['pdfjs-dist/build/pdf.worker.min.mjs']
+        host: '0.0.0.0',
+        port: 3000,
+        strictPort: true,
+        watch: {
+            usePolling: true,
+            interval: 1000 // Check la fiecare secundă
+        },
+        hmr: {
+            host: 'localhost',
+            port: 3000
+        }
     }
 });
